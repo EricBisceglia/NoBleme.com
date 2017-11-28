15a5c97fcd1ee8a045e1f2cca186ce7694502a49
Éric Bisceglia <none@none>
2017-11-28 12:21:33 +0100

    Tâche #387 : Le bot IRC devrait notifier le canal #english pour les IRL http://nobleme.com/pages/todo/index?id=387

--- pages/dev/ircbot_boot.php
+++ pages/dev/ircbot_boot.php
@@ -46,6 +46,7 @@ fputs($socket_irc, str_replace('PING','PONG',$fetchping)."\r\n");
 // Si le pong est accepté, on peut join les canaux
 fputs($socket_irc,"JOIN #NoBleme\r\n");
 fputs($socket_irc,"JOIN #dev\r\n");
+fputs($socket_irc,"JOIN #english\r\n");
 
 // Et ne pas oublier de s'indentifier pour ne pas se faire troller par nickserv
 fputs($socket_irc,"NickServ IDENTIFY $pseudo_bot $pass_bot\r\n");
